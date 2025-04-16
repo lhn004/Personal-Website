3b4c6f34a466214e03a5737ae81e1cce937a52fa
lhn004 <lhn004@bucknell.edu>
2025-04-16 08:32:14 -0400

    added tab order to resume

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -5,7 +5,7 @@ import pdf from '../../assets/Linh_Nguyen_Resume.pdf'
 const Resume = () => {
     return (
         <section id='resume'>
-            <h1 className="sectionTitle">My Resume</h1>
+            <h1 className="sectionTitle" tabIndex="0">My Resume</h1>
             <iframe src={pdf} title="Linh Nguyen Resume" width="90%" height="800px"></iframe>
         </section>
     )
